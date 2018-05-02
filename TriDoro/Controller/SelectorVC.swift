c95ae7827ca0f6cfe1505d7b09374158b9b9412f
Levente Máthé <leviouss@gmail.com>
2018-05-02 18:43:03 +0200

    Initial commit

---
+++ TriDoro/Controller/SelectorVC.swift
@@ -0,0 +1,13 @@
+//
+//  SelectorVC.swift
+//  TriDoro
+//
+//  Created by Máthé Levente on 2018. 05. 02..
+//  Copyright © 2018. Levente Máthé. All rights reserved.
+//
+
+import UIKit
+
+class SelectorVC: UIViewController {
+
+}
